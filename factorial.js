4c75692b564407e663712c6a08e5d1c213cdbbe0
Sujoy <sujoy143656@gmail.com>
2025-09-26 11:46:56 +0530

    Factorial 2 different way solve this problem

---
+++ factorial.js
@@ -0,0 +1,23 @@
+let factorial = (number) => {
+  let arr = [];
+  for (let i = number; i >= 1; i--) {
+    arr.push(i);
+  }
+  let total = arr.reduce((initialVal, currentVal) => {
+    return initialVal * currentVal;
+  },1);
+
+  return total;
+};
+
+console.log(factorial(3));
+
+let recursivefactorial = (n)=>{
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * recursivefactorial(n - 1);
+}
+
+
+console.log(recursivefactorial(4));
