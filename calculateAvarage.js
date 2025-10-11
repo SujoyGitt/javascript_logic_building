3b607196095aaf4c9292d26c1e56e6a61fe16d6f
Sujoy <sujoy143656@gmail.com>
2025-10-11 12:32:53 +0530

    solve find median problem

--- calculateAvarage.js
+++ calculateAvarage.js
@@ -1,9 +1,12 @@
 let calculateAvarage = (arr) => {
+  if (arr.length === 0) {
+   return 0; 
+  }
   let total = arr.reduce((initalVal, elem) => {
     return initalVal + elem;
   }, 0);
   return total / arr.length;
 };
 
-console.log(calculateAvarage([35, 25, 32, 87, 97, 63]));
+console.log(calculateAvarage([]));
            
---
+++ findMedian.js
@@ -0,0 +1,13 @@
+const findMedian = (arr) => {
+  let nwArr = arr.sort((a, b) => a - b);
+
+  let mid = Math.floor(arr.length / 2);
+
+  if (arr.length % 2 === 0) {
+    return (nwArr[mid] + nwArr[mid - 1]) / 2;
+  } else {
+    return nwArr[mid];
+  }
+};
+console.log(findMedian([8, 3, 2, 9, 7]));
+console.log(findMedian([5, 3, 9, 2]));
