641e1afe768eaa3ed3c2df092c56b4c9faa0ed56
Sujoy <sujoy143656@gmail.com>
2025-10-17 13:22:00 +0530

    solve 2 new task repeat string and trunckate

--- trunckate.js
+++ trunckate.js
@@ -1,4 +1,5 @@
 const trunckate = (str,leng)=>{
+ if (leng <= 0) return str;
  return str.length > leng ? str.slice(0,leng) + "..." : str;
 }
-console.log(trunckate("A Quick Brown fox jumps over the lazy dog",10))
+console.log(trunckate("A Quick Brown fox jumps over the lazy dog",0))
